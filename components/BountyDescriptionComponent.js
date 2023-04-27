2be31f371ae5404f108919ddceaef10aba575a7a
Manav Gupta <2006468@kiit.ac.in>
2023-04-27 16:20:01 +0530

    created Bounty-description component

--- components/BountyDescriptionComponent.js
+++ components/BountyDescriptionComponent.js
@@ -1,34 +1,44 @@
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import styles from "../styles/bountydescription.module.css";
-import remarkGfm from "remark-gfm";
-import { webApp } from "@/store/predefined-create-bounty-desc";
+import GetHelp from "../store/gethelp.mdx";
+import Custom from "../store/custom.mdx";
+import WebApp from "../store/webapp.mdx";
+import Tools from "../store/tool.mdx";
 
 const BountyDescription = () => {
+  const reqSkills = ["Machine Learning", "WebApp", "React"];
+  const tags = ["React", "Javascript", "node", "css"];
   return (
     <>
       <h2 className={styles.bountyType_title}>Build An App For Others</h2>
-      <div className={styles.bountyDesc_outer}>
-        <h3>Bounty Description:</h3>
-        <div className={styles.bountyDesc}>
-          <ReactMarkdown
-            className={styles.mdText}
-            children={webApp.descStruc}
-            remarkPlugins={[remarkGfm]}
-          />
-        </div>
-      </div>
       <div className={styles.bountyDesc_extras}>
         <h3>Required Skills:</h3>
-        <p>Machine Learning, WebApp, React</p>
+        <p>
+          {reqSkills.reduce((iter, skill) => {
+            return iter + ", " + skill;
+          })}
+        </p>
       </div>
       <div className={styles.bountyDesc_extras}>
         <h3>Tags:</h3>
-        <p>React, Javascript, node, css</p>
+        <p>
+          {tags.reduce((iter, skill) => {
+            return iter + ", " + skill;
+          })}
+        </p>
       </div>
       <div className={styles.bountyDesc_extras}>
         <h3>Connect With Me:</h3>
+      </div>
+      <div className={styles.bountyDesc_outer}>
+        <h3>Bounty Description:</h3>
+        <div className={styles.bountyDesc}>
+          <Custom />
+        </div>
       </div>
     </>
   );
 };
 export default BountyDescription;
+
+// Add Communication Link dynamically
+// Add Style to the page
